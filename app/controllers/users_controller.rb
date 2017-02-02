92fd42b81eea83d5b740a67fd7ec56e7e4d651b4
Charlie <charlie@myjobreel.com>
2017-02-02 01:50:37 +0000

    Add create function for work history

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
     @user.update_attributes(user_params)
     redirect_to user_path
   end
