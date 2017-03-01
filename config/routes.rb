38f29fd3eef4b08265c36f95c94307b6c13abed6
Charlie <charlie@myjobreel.com>
2017-03-01 23:21:29 +0000

    Add conditional logic to partials

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   # Going to need to use nested resources so that a work history is created for a specific user
 
   resources :users, only: [:show, :edit, :update, :destroy]
-
   resources :work_histories
   resources :videos
 end
