07188352cebc749917bdc53c16825b1ff2a58a45
Charlie <charlie@myjobreel.com>
2017-03-09 02:10:38 +0000

    Add AJAX PATCH request

--- app/controllers/jobreels_controller.rb
+++ app/controllers/jobreels_controller.rb
@@ -1,23 +1,25 @@
 class JobreelsController < ApplicationController
-end
 
-def create
-  @new_jobreel = Jobreel.new
-end
+  def create
+    @new_jobreel = Jobreel.new
+  end
 
-def edit
-end
+  def edit
+    @jobreel = Jobreel.find(params[:id])
+  end
 
-def update
-  @jobreel = Jobreel.find(params[:id])
-end
+  def update
+    byebug
+  end
 
-def destroy
-end
+  def destroy
+  end
 
 
-private
+  private
 
-def jobreel_params
-  params.require(:jobreel).permit(:user_id, :section1_videos, :section2_videos, :section3_videos, :section4_videos, :section5_videos)
+  def jobreel_params
+    params.require(:jobreel).permit(:user_id, :section1_videos, :section2_videos, :section3_videos, :section4_videos, :section5_videos)
+  end
+
 end
