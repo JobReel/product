863bba347bfff6274b1b8d18a932016a13fe55a6
Scott Stapley <scott@bigscoots.com>
2017-03-09 00:09:25 +0000

    recommednations

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update, :destroy]
   resources :work_histories
   resources :educations
+  resources :recommendations
   resources :videos
 end
