ab4663248129e05103c58f0e04ef4d2b85c2bdb9
Charlie <charlie@myjobreel.com>
2017-03-11 01:14:46 +0000

    Pass correct hash in Ajax PATCH request

--- app/controllers/jobreels_controller.rb
+++ app/controllers/jobreels_controller.rb
@@ -1,4 +1,6 @@
 class JobreelsController < ApplicationController
+  before_action :set_jobreels, only: [:update]
+  respond_to :html, :js
 
   def create
     @new_jobreel = Jobreel.new
@@ -9,7 +11,9 @@ class JobreelsController < ApplicationController
   end
 
   def update
+    @jobreel = Jobreel.find(params[:id])
     byebug
+    @jobreel.update_attributes(jobreel_params)
   end
 
   def destroy
@@ -17,6 +21,10 @@ class JobreelsController < ApplicationController
 
 
   private
+
+  def set_jobreels
+    @jobreel = Jobreel.find(params[:id])
+  end
 
   def jobreel_params
     params.require(:jobreel).permit(:user_id, :section1_videos, :section2_videos, :section3_videos, :section4_videos, :section5_videos)
