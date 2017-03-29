fc415723091e05659701e32da76e65ad033f3615
Charlie O'Brien <charlie@myjobreel.com>
2017-03-29 23:56:43 +0000

    clean up code, still not working

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    root_path # Or :prefix_to_your_route
+    root_path
   end
 end
