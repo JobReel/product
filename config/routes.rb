4e9df0aa9d52d944565c81d7d05a5a8463f72d50
Charlie O'Brien <charlie@myjobreel.com>
2017-03-31 17:18:48 +0000

    Add static page for /employer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: "sessions", registrations: "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
+  get '/employer' => 'pages#employer'
   # Going to need to use nested resources so that a work history is created for a specific user
 
   resources :users, only: [:show, :edit, :update, :destroy]
