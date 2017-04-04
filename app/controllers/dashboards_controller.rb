a2b5ebfb58a75f8132051aa2cce2f0793cd6dc4c
Charlie O'Brien <charlie@myjobreel.com>
2017-04-04 21:49:57 +0000

    Page created and hooked up to routes

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,13 @@
 class DashboardsController < ApplicationController
 
   def show
+    @user = current_user
+    @workhistory = WorkHistory.where(user_id: current_user.id)
+    @videos = Video.where(user_id: current_user.id)
+    @jobreels = Jobreel.where(user_id: current_user.id)
+    @jobs = Job.where(user_id: current_user.id)
+    @recs = Recommendation.where(user_id: current_user.id)
+    byebug
   end
 
 end
