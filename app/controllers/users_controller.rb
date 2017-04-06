6c650e98b103403c3895b7fd7bfe19fe554629e6
Charlie O'Brien <charlie@myjobreel.com>
2017-04-06 17:38:14 +0000

    Helper method for user video

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     else
     end
     @avatar = display_avatar(@user)
+    @video_avatar = display_video(@user)
   end
 
   def edit
@@ -34,6 +35,14 @@ private
       return '<img src="http://res.cloudinary.com/jobreel/image/upload/v1491256810/default_avatar.png" alt="Default avatar">'
     else
       return '<img src="http://res.cloudinary.com/jobreel/image/upload/c_thumb,g_face,h_40,r_20,w_40/v'+ user.image.stored_version + '/' + user.first_name + '.png" alt="User Avatar">'
+    end
+  end
+
+  def display_video(user)
+    if user.video.blank?
+      return '<img src="http://res.cloudinary.com/jobreel/video/upload/c_fill,h_72,w_100/poatoe.jpg" alt="Default Video">'
+    else
+      return '<img src="http://res.cloudinary.com/jobreel/video/upload/c_fill,h_72,w_100/' + user.first_name + '.jpg" alt="User Video">'
     end
   end
 
