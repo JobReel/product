7c1dd299b366496a39c1e7240169c34884cf975d
Charlie O'Brien <charlie@myjobreel.com>
2017-04-06 17:05:20 +0000

    Moved logic to controller, output html instead of using cl_image_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
       @user = User.new
     else
     end
+    @avatar = display_avatar(@user)
   end
 
   def edit
@@ -27,5 +28,14 @@ private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :title, :city, :state, :bio, :degree_type, :degree_field, :image, :video)
   end
+
+  def display_avatar(user)
+    if user.image.blank?
+      return '<img src="http://res.cloudinary.com/jobreel/image/upload/v1491256810/default_avatar.png" alt="Default avatar">'
+    else
+      return '<img src="http://res.cloudinary.com/jobreel/image/upload/c_thumb,g_face,h_40,r_20,w_40/v'+ user.image.stored_version + '/' + user.first_name + '.png" alt="User Avatar">'
+    end
+  end
+
 
 end
