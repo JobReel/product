d3ef5cb0b79a4c97af9e9312a5b1044d7bc575ce
Charlie O'Brien <charlie@myjobreel.com>
2017-04-11 00:16:17 +0000

    update the signed in navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,21 +30,4 @@ private
     params.require(:user).permit(:first_name, :last_name, :title, :city, :state, :bio, :degree_type, :degree_field, :image, :video)
   end
 
-  def display_avatar(user)
-    if user.image.blank?
-      return '<img src="http://res.cloudinary.com/jobreel/image/upload/v1491256810/default_avatar.png" alt="Default avatar">'
-    else
-      return '<img src="http://res.cloudinary.com/jobreel/image/upload/c_thumb,g_face,h_40,r_20,w_40/v'+ user.image.stored_version + '/' + user.first_name + '.png" alt="User Avatar">'
-    end
-  end
-
-  def display_video(user)
-    if user.video.blank?
-      return '<img src="http://res.cloudinary.com/jobreel/video/upload/c_fill,h_72,w_100/poatoe.jpg" alt="Default Video">'
-    else
-      return '<img src="http://res.cloudinary.com/jobreel/video/upload/c_fill,h_72,w_100/' + user.first_name + '.jpg" alt="User Video">'
-    end
-  end
-
-
 end
