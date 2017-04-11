99857d26f36f2400d7469cd1a69d9c1891f5ebbc
Charlie O'Brien <charlie@myjobreel.com>
2017-04-11 19:19:00 +0000

    Redirect user to dashboard from index if signed in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   def index
     # redirect to dashboard if user is signed in already
     # http://stackoverflow.com/questions/14416234/devise-redirect-automatically-from-root-url-if-signed-in
+    if user_signed_in?
+      redirect_to dashboards_path
+    end
   end
 
   def employer
