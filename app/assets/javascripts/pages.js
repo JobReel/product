15ddd5d0c23d4d29674fa57a76031f9c9847dafe
Scott Stapley <scott@bigscoots.com>
2017-06-28 23:24:14 +0000

    login-page-js-update

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,52 +1,64 @@
-$(document).ready(function(){
+$(document).on('turbolinks:load', function() {
     $("#signup-tab").click(function(e){
         $("#signup-tab").attr('class', 'col-6 vert-align signup-tab-active');
         $("#login-tab").attr('class', 'col-6 vert-align login-tab-inactive');
-        $("#welcome-bar").html('<br /><span class="big-blue">START TODAY!</span><br />');
+        if(window.location.href.indexOf("employer") > -1) {
+          $("#welcome-bar").html('<br /><span class="big-blue">START TODAY EMPLOYER!</span><br />');
+        } else {
+          $("#welcome-bar").html('<br /><span class="big-blue">START TODAY JOB SEEKER!</span><br />');
+        };
         $("#signup-form-box").attr('class', 'col-12 login-form-box');
         $("#login-form-box").attr('class', 'col-12 signup-form-box');
     });
     $("#signup-tab-text").click(function(e){
         $("#signup-tab").attr('class', 'col-6 vert-align signup-tab-active');
         $("#login-tab").attr('class', 'col-6 vert-align login-tab-inactive');
-        $("#welcome-bar").html('<br /><span class="big-blue">START TODAY!</span><br />');
+        if(window.location.href.indexOf("employer") > -1) {
+          $("#welcome-bar").html('<br /><span class="big-blue">START TODAY EMPLOYER!</span><br />');
+        } else {
+          $("#welcome-bar").html('<br /><span class="big-blue">START TODAY JOB SEEKER!</span><br />');
+        };
         $("#signup-form-box").attr('class', 'col-12 login-form-box');
         $("#login-form-box").attr('class', 'col-12 signup-form-box');
     });
     $("#login-tab").click(function(e){
         $("#login-tab").attr('class', 'col-6 vert-align login-tab-active');
         $("#signup-tab").attr('class', 'col-6 vert-align signup-tab-inactive');
-        $("#welcome-bar").html('<br /><span class="big-blue">WELCOME BACK!</span><br />');
+        if(window.location.href.indexOf("employer") > -1) {
+          $("#welcome-bar").html('<br /><span class="big-blue">WELCOME BACK EMPLOYER!</span><br />');
+        } else {
+          $("#welcome-bar").html('<br /><span class="big-blue">WELCOME BACK JOB SEEKER!</span><br />');
+        };
         $("#signup-form-box").attr('class', 'col-12 signup-form-box');
         $("#login-form-box").attr('class', 'col-12 login-form-box');
     });
     $("#login-tab-text").click(function(e){
         $("#login-tab").attr('class', 'col-6 vert-align login-tab-active');
         $("#signup-tab").attr('class', 'col-6 vert-align signup-tab-inactive');
-        $("#welcome-bar").html('<br /><span class="big-blue">WELCOME BACK!</span><br />');
+        if(window.location.href.indexOf("employer") > -1) {
+          $("#welcome-bar").html('<br /><span class="big-blue">WELCOME BACK EMPLOYER!</span><br />');
+        } else {
+          $("#welcome-bar").html('<br /><span class="big-blue">WELCOME BACK JOB SEEKER!</span><br />');
+        };
         $("#signup-form-box").attr('class', 'col-12 signup-form-box');
         $("#login-form-box").attr('class', 'col-12 login-form-box');
     });
-
-});
-
-$(document).ready(function(){
-  $("form#login-box").bind ("ajax:success", function(e, data, status, xhr){
-    window.location.replace("http://localhost:3030/dashboards/");
-  }
-  );
-  $("form#login-box").bind ("ajax:error", function(e, data, status, xhr){
-    sweetAlert("Oops...", "Please try again!", "error");
-  }
-  );
-  $("form#signup-box").bind ("ajax:success", function(e, data, status, xhr){
-   if (data.success) {
-    //javascript that executes if everything goes o.k.;
-    //location.reload();
-    window.location.replace("http://localhost:3030/dashboards/");
-  }
-   else {
-    sweetAlert("Oops...", "Please try again!", "error");
-  }
-  });
+    $("form#login-box").bind ("ajax:success", function(e, data, status, xhr){
+      window.location.replace("http://localhost:3030/dashboards/");
+    }
+    );
+    $("form#login-box").bind ("ajax:error", function(e, data, status, xhr){
+      sweetAlert("Oops...", "Please Try Again!", "error");
+    }
+    );
+    $("form#signup-box").bind ("ajax:success", function(e, data, status, xhr){
+     if (data.success) {
+      //javascript that executes if everything goes o.k.;
+      //location.reload();
+      window.location.replace("http://localhost:3030/dashboards/");
+    }
+     else {
+      sweetAlert("Oops...", "Please Try Again!", "error");
+    }
+    });
 });
