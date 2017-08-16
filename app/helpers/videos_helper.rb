77fcd97e816b55acb623b04bae7d8955710c2346
Charlie O'Brien <charlie@myjobreel.com>
2017-08-16 14:58:45 +0000

    Update controllers for multiclip sections

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -5,8 +5,18 @@ module VideosHelper
   end
 
   def jobreel_default_intro(video)
+
     return cl_video_tag("default_intro", controls: true) if video.nil?
-    cl_video_tag(video, controls: true, transformation: @overlays)
+    jsonvideo = JSON.parse(video)
+    ev = []
+    firstvid = jsonvideo.shift
+    jsonvideo.each do |vid|
+      ev << {:overlay=>"video:" + vid, :flags=>"splice", :width=>333, :height=>188, :crop=>"fill"}
+    end
+    overlays = [{:width=>333, :height=>188, :crop=>"fill"}]
+    overlays << ev
+    overlays.flatten!
+    cl_video_tag(firstvid, controls: true, transformation: overlays)
   end
 
 end
