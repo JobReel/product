715ab6740a33b297b2dc0de2ccd2fb93b4bb0442
Charlie O'Brien <charlie@myjobreel.com>
2017-08-28 18:34:17 +0000

    Smooth expand js

--- app/assets/javascripts/jobs-js.js
+++ app/assets/javascripts/jobs-js.js
@@ -1,19 +1,20 @@
 
 $(document).on('turbolinks:load', function(){
-  $('#full-description').css({height:'200px', overflow:'hidden'});
+  fullH = document.getElementById("full-description").scrollHeight;
   $('#collapseme').on('click', function() {
-      var $this = $(this.previousElementSibling);
-      if ($this.data('open')) {
-          $('#full-description').switchClass("job-description", "job-description-full");
-          $this.animate({height:'200px'});
-          $this.data('open', 0);
-
-      }
-      else {
-          $('#full-description').switchClass("job-description-full", "job-description");
-          $this.animate({height:'100%'} );
-          $this.data('open', 1);
-
-      }
+    var duration = 1000,
+    easing = 'swing';
+              if (160 == $('#full-description').height()) {
+                  $('#full-description').animate(
+                      {
+                          'height' : fullH + 60
+                      }, duration, easing);
+              }
+              else {
+                  $('#full-description').animate(
+                      {
+                          'height' : 200
+                      }, duration, easing);
+              }
   });
 });
