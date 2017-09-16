f7cbaf76d184045fce7d70f3dacb5f801e81e763
Charlie O'Brien <charlie@myjobreel.com>
2017-09-16 19:42:13 +0000

    Fix seeding

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,6 @@ class Question < ApplicationRecord
   belongs_to :job, optional: true
 
   validates :text, presence: true
-  validates :comptetency_id, presence: true
+  validates :competency_id, presence: true
 
 end
