351d30226baa1bde806d2755c3fcd480eee16adb
scott s <sstaple2@binghamton.edu>
2017-11-08 18:54:56 +0000

    settings page done

--- app/assets/javascripts/dashboards-js.js
+++ app/assets/javascripts/dashboards-js.js
@@ -1,116 +1,117 @@
 $(document).on('turbolinks:load', function(){
-
-  var inputspassword = document.getElementById('password-update-form');
-  for(var i=0; i<inputspassword.length; i++){
-    if(inputspassword[i].type == "password"){
-      inputspassword[i].onkeyup = doPasswordCheck;
-      inputspassword[i].onblur = doPasswordCheck;
-    }
-  };
-
-  var inputs = document.getElementById('email-update-form');
-  for(var i=0; i<inputs.length; i++){
-    if(inputs[i].type == "password"){
-      inputs[i].onkeyup = doCheck;
-      inputs[i].onblur = doCheck;
-    }
-  };
-
-// email form success
-  $("form#email-update-form").bind ("ajax:success", function(e, data, status, xhr){
-    swal({
-      title: 'Your email has been updated!',
-      type: 'success',
-      showConfirmButton: false,
-      timer: 1500
-    });
-    window.location.reload();
-  });
-
-// email form error
-  $("form#email-update-form").bind ("ajax:error", function(e, data, status, xhr){
-    if (data.responseJSON.data.length > 1) {
-      errormessages = data.responseJSON.data[0] + " & " + data.responseJSON.data[1];
-    } else {
-     errormessages = data.responseJSON.data[0];
-    }
-    
-    document.getElementById('user_current_password').value = "";
-    document.getElementById('user_email').value = document.getElementById('user_email_placeholder').value;
-        
-    swal({
-      title: 'Oops...',
-      type: "error",
-      html: errormessages,
-      onClose: function () {
-        document.getElementById('email-update-button').disabled = true;    
+  if ($('body').hasClass("dashboards-settings")) {
+    var inputspassword = document.getElementById('password-update-form');
+    for(var i=0; i<inputspassword.length; i++){
+      if(inputspassword[i].type == "password"){
+        inputspassword[i].onkeyup = doPasswordCheck;
+        inputspassword[i].onblur = doPasswordCheck;
       }
-    });
-  });
+    };
 
-// password form success
-  $("form#password-update-form").bind ("ajax:success", function(e, data, status, xhr){
-    swal({
-      title: 'Your password has been updated!',
-      type: 'success',
-      showConfirmButton: false,
-      timer: 1500
-    });
-    window.location.reload();
-  });
-
-// password form error
-  $("form#password-update-form").bind ("ajax:error", function(e, data, status, xhr){
-    if (data.responseJSON.data.length > 1) {
-      errormessages = data.responseJSON.data[0] + " & " + data.responseJSON.data[1];
-    } else {
-     errormessages = data.responseJSON.data[0];
-    }
-
-// setting all password form fields to blank
-    var elements = document.getElementById("password-update-form");
-    for (var ii=0; ii < elements.length; ii++) {
-      if (elements[ii].type == "password") {
-        elements[ii].value = "";
-      }
-    }
-
-    swal({
-      title: 'Oops...',
-      type: "error",
-      html: errormessages,
-      onClose: function () {
-        document.getElementById('password-update-button').disabled = true;    
-      }
-    });
-  });
-
-// Checking for text box entries to enable update button
-  function doCheck(){
-    var allFilled = true;
-      
     var inputs = document.getElementById('email-update-form');
-    for(var i=0; i<inputs.length; i++) {
-      if(inputs[i].type == "password" && inputs[i].value == ''){
-        allFilled = false;
-        break;
+    for(var i=0; i<inputs.length; i++){
+      if(inputs[i].type == "password"){
+        inputs[i].onkeyup = doCheck;
+        inputs[i].onblur = doCheck;
       }
-    }
-    document.getElementById("email-update-button").disabled = !allFilled;
-  };
+    };
 
-  function doPasswordCheck(){
-    var allFilled = true;
-      
-    var inputs = document.getElementById('password-update-form');
-    for(var i=0; i<inputs.length; i++) {
-      if(inputs[i].type == "password" && inputs[i].value == ''){
-        allFilled = false;
-        break;
+  // email form success
+    $("form#email-update-form").bind ("ajax:success", function(e, data, status, xhr){
+      swal({
+        title: 'Your email has been updated!',
+        type: 'success',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      window.location.reload();
+    });
+
+  // email form error
+    $("form#email-update-form").bind ("ajax:error", function(e, data, status, xhr){
+      if (data.responseJSON.data.length > 1) {
+        errormessages = data.responseJSON.data[0] + " & " + data.responseJSON.data[1];
+      } else {
+       errormessages = data.responseJSON.data[0];
       }
-    }
-    document.getElementById("password-update-button").disabled = !allFilled;
-  };
+      
+      document.getElementById('user_current_password').value = "";
+      document.getElementById('user_email').value = document.getElementById('user_email_placeholder').value;
+          
+      swal({
+        title: 'Oops...',
+        type: "error",
+        html: errormessages,
+        onClose: function () {
+          document.getElementById('email-update-button').disabled = true;    
+        }
+      });
+    });
+
+  // password form success
+    $("form#password-update-form").bind ("ajax:success", function(e, data, status, xhr){
+      swal({
+        title: 'Your password has been updated!',
+        type: 'success',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      window.location.reload();
+    });
+
+  // password form error
+    $("form#password-update-form").bind ("ajax:error", function(e, data, status, xhr){
+      if (data.responseJSON.data.length > 1) {
+        errormessages = data.responseJSON.data[0] + " & " + data.responseJSON.data[1];
+      } else {
+       errormessages = data.responseJSON.data[0];
+      }
+
+  // setting all password form fields to blank
+      var elements = document.getElementById("password-update-form");
+      for (var ii=0; ii < elements.length; ii++) {
+        if (elements[ii].type == "password") {
+          elements[ii].value = "";
+        }
+      }
+
+      swal({
+        title: 'Oops...',
+        type: "error",
+        html: errormessages,
+        onClose: function () {
+          document.getElementById('password-update-button').disabled = true;    
+        }
+      });
+    });
+
+  // Checking for text box entries to enable update button
+    function doCheck(){
+      var allFilled = true;
+        
+      var inputs = document.getElementById('email-update-form');
+      for(var i=0; i<inputs.length; i++) {
+        if(inputs[i].type == "password" && inputs[i].value == ''){
+          allFilled = false;
+          break;
+        }
+      }
+      document.getElementById("email-update-button").disabled = !allFilled;
+    };
+
+    function doPasswordCheck(){
+      var allFilled = true;
+        
+      var inputs = document.getElementById('password-update-form');
+      for(var i=0; i<inputs.length; i++) {
+        if(inputs[i].type == "password" && inputs[i].value == ''){
+          allFilled = false;
+          break;
+        }
+      }
+      document.getElementById("password-update-button").disabled = !allFilled;
+    };
+  }
 });
 
 function cloudAvatar() {
