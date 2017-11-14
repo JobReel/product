edf7ce6282dc16c5f1003575f43b775c9c3be6b9
Charlie O'Brien <charlie@myjobreel.com>
2017-11-14 23:53:49 +0000

    more setup

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -9,4 +9,10 @@ class ApplicationsController < ApplicationController
   def destroy
   end
 
+ private
+
+  def application_params
+    params.require(:application).permit(:user_id, :job_id, :jobreel_id)
+  end
+
 end
