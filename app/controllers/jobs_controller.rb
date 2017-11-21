103705a75d123d4270e89e58173fc005124f766a
Charlie O'Brien <charlie@myjobreel.com>
2017-11-21 18:34:42 +0000

    move jobreel create to model

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -27,21 +27,6 @@ before_action :authenticate_user!, only: [:new, :show, :create, :edit, :update, 
     @job.team_ids.push(current_user.id)
     @job.save!
 
-    @jobreel = Jobreel.new()
-    @jobreel.user_id = current_user.id
-    @jobreel.job_id = Job.last.id
-    @jobreel.section1_title = "About the Company"
-    @jobreel.section2_title = "Challenges"
-    @jobreel.section3_title = "Expectations"
-    @jobreel.section4_title = "Team"
-    @jobreel.section5_title = "Perks & Benefits"
-    @jobreel.section1_duration = 0
-    @jobreel.section2_duration = 0
-    @jobreel.section3_duration = 0
-    @jobreel.section4_duration = 0
-    @jobreel.section5_duration = 0
-    @jobreel.save!
-
     render json: @job
 
   end
--- app/models/job.rb
+++ app/models/job.rb
@@ -6,4 +6,24 @@ class Job < ApplicationRecord
   has_many :competencies
   has_many :questions
 
+  after_create :create_jobreel_for_job!
+
+
+  def create_jobreel_for_job!
+    @jobreel = Jobreel.new()
+    @jobreel.user_id = current_user.id
+    @jobreel.job_id = Job.last.id
+    @jobreel.section1_title = "About the Company"
+    @jobreel.section2_title = "Challenges"
+    @jobreel.section3_title = "Expectations"
+    @jobreel.section4_title = "Team"
+    @jobreel.section5_title = "Perks & Benefits"
+    @jobreel.section1_duration = 0
+    @jobreel.section2_duration = 0
+    @jobreel.section3_duration = 0
+    @jobreel.section4_duration = 0
+    @jobreel.section5_duration = 0
+    @jobreel.save!
+  end
+
 end
