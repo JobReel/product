f625531742653f749543439c0a3ab39fb70ad0a7
Charlie O'Brien <charlie@myjobreel.com>
2017-11-22 16:20:48 +0000

    temp stashing of changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/team' => 'dashboards#team'
   get '/settings' => 'dashboards#settings'
   get '/applicationAPI' => 'jobreels#instant_apply'
+  get '/addtoteamAPI' =>'dashboards#addtoteamAPI'
   # Going to need to use nested resources so that a work history is created for a specific user
 
   resources :users, only: [:show, :edit, :update, :destroy]
